368f64e4879c238ddf358fd992628553a5a654fd
Matt DeMonaco <mdemonaco@gmail.com>
2019-10-27 19:08:21 -0700

    added timer variable tied to a function to countdown clock

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,47 @@
+// global variables
+var correct = 0;
+var wrong = 0;
+var noAnswer = 0;
+var hiddenContainer = $("#mainContainer2").hide();
+var results = $("#mainContainer3").hide();
+var count = 120;
+var timeStart = function() {
+    $("#time").html(count);
+setInterval(startTime, 1000);
+function startTime(){
+    $("#time").html(count);
+    count--; 
+};
+};
+
+
 // Allow the doc to be loaded and ready
 // when the button is clicked to submit questions, the main container div will be toggled
 
 $(document).ready(function() {
+//set elements to first be hidden before click actions
     $("#mainContainer2").hide();
     $("#mainContainer3").hide();
-
+//start game div will be removed and question set will be shown
     $("#startGame").click(function() {
         console.log("hello!");
      $("#mainContainer1").remove();
      $("#mainContainer2").show();
-
+    if ($("#maincontainer2").show === true);
+     console.log("this is true");
+     timeStart();
+     if (count == 115);
+     $("#mainContainer2").hide();
+     $("#mainContainer3").show();
+     
+//question set will be removed and results page will be shown
     $("#submit").click(function(){
         $("#mainContainer3").hide();
         $("#mainContainer2").remove();
         $("#mainContainer3").show();
-    });
+         
+
+    
 });
 });
-    // $("#startGame").click(function(){
-    //     $("#mainContainer2").toggle("show");
-
-//  if ($("#mainConainter2").val() === "hidden"){
-//      $("#mainContainer2").show();
-//  }
-
-
-       
-    //    if($("#mainContainer").is(":hidden")) {
-    //     $("#mainContainer2").show;
-
-    //    }
-    //    $("#startGame").click(function(){
-    //     $("#mainContainer2").show("fast");
-    //    });
-
-
-// setInterval( , 1000);
-
-
-
-// global variables
-var correct = 0
-var wrong = 0
-var noAnswer = 0
-var hiddenContainer = $("#mainContainer2").hide();
-var results = $("#mainContainer3").hide();
-// var mainContainer = false
+});
